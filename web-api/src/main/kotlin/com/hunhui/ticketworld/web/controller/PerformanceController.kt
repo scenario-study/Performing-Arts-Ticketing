e3ae3b7a79e54ffddd1bc7eea4012964d8511867
JeongHunHui <deca3248@gmail.com>
2025-01-13 19:18:05 +0900

    feat: 공연 관련 API Controller 구현

---
+++ web-api/src/main/kotlin/com/hunhui/ticketworld/web/controller/PerformanceController.kt
@@ -0,0 +1,37 @@
+package com.hunhui.ticketworld.web.controller
+
+import com.hunhui.ticketworld.application.PerformanceService
+import com.hunhui.ticketworld.application.dto.request.PerformanceCreateRequest
+import com.hunhui.ticketworld.application.dto.response.PerformanceResponse
+import com.hunhui.ticketworld.application.dto.response.PerformanceSummaryListResponse
+import com.hunhui.ticketworld.web.controller.doc.PerformanceApiDoc
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController("/performance")
+class PerformanceController(
+    private val performanceService: PerformanceService,
+) : PerformanceApiDoc {
+    @GetMapping("/{performanceId}")
+    override fun getPerformance(
+        @PathVariable("performanceId") performanceId: UUID,
+    ): ResponseEntity<PerformanceResponse> = ResponseEntity.ok(performanceService.getPerformance(performanceId))
+
+
+    @GetMapping
+    override fun getPerformanceSummaryList(
+        @RequestParam(defaultValue = "10") size: Int,
+        @RequestParam(defaultValue = "0") page: Int,
+    ): ResponseEntity<PerformanceSummaryListResponse> = ResponseEntity.ok(performanceService.getPerformances(page, size))
+
+    @PostMapping
+    override fun savePerformance(
+        @RequestBody performanceCreateRequest: PerformanceCreateRequest,
+    ): ResponseEntity<Unit> = ResponseEntity.ok(performanceService.savePerformance(performanceCreateRequest))
+}
