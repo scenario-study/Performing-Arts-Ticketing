83caf2ce6d0d3136a7318a8ba2c2e5639dba55c8
CodeDiary18 <codediary18@gmail.com>
2025-01-15 03:39:29 +0900

    chore: querydsl setting

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -20,7 +20,10 @@ val queryDslVersion: String by extra
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-hibernate5:2.18.0")
+
+    // querydsl
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     kapt("jakarta.annotation:jakarta.annotation-api")
@@ -34,12 +37,12 @@ dependencies {
 
 val generated = file("src/main/generated")
 tasks.withType<JavaCompile> {
-    options.generatedSourceOutputDirectory.set(generated)
+    options.generatedSourceOutputDirectory = generated
 }
 
 sourceSets {
     main {
-        kotlin.srcDirs += generated
+        kotlin.srcDirs += file("${projectDir}/build/generated")
     }
 }
 
---
+++ infra/src/main/kotlin/com/cd18/infra/persistence/config/QueryDslConfig.kt
@@ -0,0 +1,16 @@
+package com.cd18.infra.persistence.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueryDslConfig(
+    val em: EntityManager,
+) {
+    @Bean
+    fun queryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(em)
+    }
+}
