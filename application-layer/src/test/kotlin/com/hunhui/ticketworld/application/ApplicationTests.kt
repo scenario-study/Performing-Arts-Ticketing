a68fa935f712130fabc8db1f19b3d3614083a4d9
JeongHunHui <deca3248@gmail.com>
2025-01-15 15:32:55 +0900

    fix: web-api 모듈을 제외하고 SpringBootTest 어노테이션 제거

--- application-layer/src/test/kotlin/com/hunhui/ticketworld/application/ApplicationTests.kt
+++ application-layer/src/test/kotlin/com/hunhui/ticketworld/application/ApplicationTests.kt
@@ -1,9 +1,7 @@
 package com.hunhui.ticketworld.application
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ApplicationTests {
     @Test
     fun contextLoads() {
--- infra-layer/src/test/kotlin/com/hunhui/ticketworld/infra/InfraTests.kt
+++ infra-layer/src/test/kotlin/com/hunhui/ticketworld/infra/InfraTests.kt
@@ -1,9 +1,7 @@
 package com.hunhui.ticketworld.infra
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class InfraTests {
     @Test
     fun contextLoads() {
