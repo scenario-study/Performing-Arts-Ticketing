82331d6bf496c1e4579f79fe287837ec7642c6d0
JeongHunHui <deca3248@gmail.com>
2025-01-16 20:21:43 +0900

    style: 포맷팅

--- domain-layer/performance-context/src/main/kotlin/com/hunhui/ticketworld/domain/performance/exception/InvalidPerformanceException.kt
+++ domain-layer/performance-context/src/main/kotlin/com/hunhui/ticketworld/domain/performance/exception/InvalidPerformanceException.kt
@@ -2,4 +2,6 @@ package com.hunhui.ticketworld.domain.performance.exception
 
 import com.hunhui.ticketworld.common.error.BusinessException
 
-class InvalidPerformanceException(performanceErrorCode: PerformanceErrorCode) : BusinessException(performanceErrorCode)
+class InvalidPerformanceException(
+    performanceErrorCode: PerformanceErrorCode,
+) : BusinessException(performanceErrorCode)
--- domain-layer/performance-context/src/main/kotlin/com/hunhui/ticketworld/domain/performance/exception/InvalidPerformanceRoundException.kt
+++ domain-layer/performance-context/src/main/kotlin/com/hunhui/ticketworld/domain/performance/exception/InvalidPerformanceRoundException.kt
@@ -2,4 +2,6 @@ package com.hunhui.ticketworld.domain.performance.exception
 
 import com.hunhui.ticketworld.common.error.BusinessException
 
-class InvalidPerformanceRoundException(performanceRoundErrorCode: PerformanceRoundErrorCode) : BusinessException(performanceRoundErrorCode)
+class InvalidPerformanceRoundException(
+    performanceRoundErrorCode: PerformanceRoundErrorCode,
+) : BusinessException(performanceRoundErrorCode)
